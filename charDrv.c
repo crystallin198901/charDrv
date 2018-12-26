cfc7483b5c0d512bf7f7bdd4d94f342bf2f157c5
crystallin198901 <crystal.lin.feng.1@gmail.com>
2018-12-26 01:12:23 -0800

    test ok

--- charDrv.c
+++ charDrv.c
@@ -1,3 +1,4 @@
+#include <linux/slab.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/fs.h>
@@ -21,7 +22,7 @@ static ssize_t chrdrv_write(struct file *f,const char __user *b,size_t c,loff_t 
   return 0;
 }
 
-static ssize_t chrdrv_read(struct file *f,const char __user *b,size_t c,loff_t *l)
+static ssize_t chrdrv_read(struct file *f,char __user *b,size_t c,loff_t *l)
 {
   printk(KERN_INFO "chrDrv read function\n");
   return 0;
@@ -31,7 +32,7 @@ static int __init chrdrv_init(void)
 {
   dev_num = MKDEV(REG_MAJ,REG_MIN);
   printk(KERN_INFO "charDrv init\n");
-  if(OK == register_chrdev_region(dev_num,10,"char device"))
+  if(0 == register_chrdev_region(dev_num,10,"char device"))
   {
     printk(KERN_INFO "device's number is %d\n",dev_num);
   }
@@ -46,7 +47,7 @@ static int __init chrdrv_init(void)
   pfo->write = chrdrv_write;
   pfo->read = chrdrv_read;
   cdev_init(pcdev,pfo);
-  if(OK == cdev_add(pcdev,dev_num,10)
+  if(0 == cdev_add(pcdev,dev_num,10))
   {
     printk(KERN_INFO "charDrv added\n");
   }
@@ -62,8 +63,8 @@ static void __exit chrdrv_exit(void)
 {
   printk(KERN_INFO "charDrv exit\n");
   cdev_del(pcdev);
-  kzfree(pcdev);
-  kzfree(pfo);
+  kfree(pcdev);
+  kfree(pfo);
   unregister_chrdev_region(dev_num,10);
 }
 
